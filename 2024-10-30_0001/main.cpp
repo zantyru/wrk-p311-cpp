77e8b322a3ddca709657e067d3dc06c791aa7706
Evgeny Zaonegin <zaonegin_s@mail.ru>
2024-10-30 18:39:02 +0300

    Update example

--- 2024-10-30_0001/main.cpp
+++ 2024-10-30_0001/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using std::cout;
 
 
 class Bottle
@@ -21,22 +22,29 @@ public:
         return _liquidVolume;
     }
 
-    void Pour(int volume)
+    int Pour(int volume)
     {
+        int pouredVolume{};
+        int availableVolume{ _totalVolume - _liquidVolume };
+
         if (volume < 0) volume = 0;
 
-        _liquidVolume += volume;
+        pouredVolume = (volume < availableVolume) ? volume : availableVolume;
+        _liquidVolume += pouredVolume;
 
-        if (_liquidVolume > _totalVolume) _liquidVolume = _totalVolume;
+        return pouredVolume;
     }
 
-    void Drain(int volume)
+    int Drain(int volume)
     {
+        int drainedVolume{};
+
         if (volume < 0) volume = 0;
 
-        _liquidVolume -= volume;
+        drainedVolume = (volume < _liquidVolume) ? volume : _liquidVolume;
+        _liquidVolume -= drainedVolume;
 
-        if (_liquidVolume < 0) _liquidVolume = 0;
+        return drainedVolume;
     }
 
 private:
@@ -47,7 +55,39 @@ private:
 
 int main()
 {
-    Bottle b;
+    Bottle b{ 10 };
+
+    cout << "\n[1]\n";
+    cout << "b total volume: " << b.GetTotalVolume() << "\n";
+    cout << "b liquid volume: " << b.GetLiquidVolume() << "\n";
+
+    int pouredVolume = b.Pour(7);
+
+    cout << "\n[2]\n";
+    cout << "b total volume: " << b.GetTotalVolume() << "\n";
+    cout << "b liquid volume: " << b.GetLiquidVolume() << "\n";
+    cout << "b poured volume: " << pouredVolume << "\n";
+
+    pouredVolume = b.Pour(4);
+
+    cout << "\n[3]\n";
+    cout << "b total volume: " << b.GetTotalVolume() << "\n";
+    cout << "b liquid volume: " << b.GetLiquidVolume() << "\n";
+    cout << "b poured volume: " << pouredVolume << "\n";
+
+    int drainedVolume = b.Drain(1);
+
+    cout << "\n[4]\n";
+    cout << "b total volume: " << b.GetTotalVolume() << "\n";
+    cout << "b liquid volume: " << b.GetLiquidVolume() << "\n";
+    cout << "b drained volume: " << drainedVolume << "\n";
+
+    drainedVolume = b.Drain(100);
+
+    cout << "\n[5]\n";
+    cout << "b total volume: " << b.GetTotalVolume() << "\n";
+    cout << "b liquid volume: " << b.GetLiquidVolume() << "\n";
+    cout << "b drained volume: " << drainedVolume << "\n";
 
     
 
