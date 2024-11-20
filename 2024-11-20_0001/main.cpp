a0d1c590993cf72564f2a620f0833c2cd115ce57
Evgeny Zaonegin <zaonegin_s@mail.ru>
2024-11-20 17:40:10 +0300

    Add new example

---
+++ 2024-11-20_0001/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using std::cout;
+
+
+class Dimension
+{
+public:
+	float Value;
+
+	Dimension(const std::string& name)
+		: Value{ 0.0f }
+		, _name{ name }
+	{ }
+
+	const std::string &GetName() const
+	{
+		return _name;
+	}
+
+	Dimension operator*(const Dimension& other) const
+	{
+		Dimension result("Результат");
+		result.Value = Value * other.Value;
+
+		return *this;
+	}
+
+private:
+	std::string _name;
+};
+
+
+int ComputeVolume(int a, int b, int c)
+{
+	return a * b * c;
+}
+
+
+double ComputeVolume(double a, double b, double c)
+{
+	return a * b * c;
+}
+
+
+Dimension ComputeVolume(Dimension a, Dimension b, Dimension c)
+{
+	return a * b * c;
+}
+
+
+int main()
+{
+	cout << ComputeVolume(34, 2, 10) << "\n";
+	cout << ComputeVolume(2.5, 3.0, 1.0) << "\n";
+
+	Dimension width("Ширина");
+	width.Value = 10.0f;
+
+	Dimension length("Длина");
+	length.Value = 2.0f;
+
+	Dimension height("Высота");
+	height.Value = 2.0f;
+
+	cout << ComputeVolume(width, length, height).Value << "\n";
+
+	return 0;
+}
